ed091f4e5bc8f8d6c76e419252021f95762f7b76
LucaBlackDragon <lucablackdragon@hotmail.com>
2020-04-04 19:56:52 +0200

    Migliorata gestione uscita dal programma

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const node = {
 }
 const hasha = require('hasha')
 const pacote = require('pacote')
+const prompts = require('prompts')
 
 require('yargs')
   .scriptName('luk3')
@@ -42,56 +43,80 @@ require('yargs')
 
 async function bootstrap({ token, path, force }) {
 
-  const cwd = path && node.path.isAbsolute(path)
-    ? path
-    : node.path.join(node.process.cwd(), path)
-
-  const a4t1Folder = node.path.join(cwd, '.a4t1/')
-
-  let authToken = undefined
   try {
-    authToken = token || node.fs.readFileSync(node.path.join(cwd, 'LUK3_TOK3N'), { encoding: 'utf8' })
+
+    const cwd = path && node.path.isAbsolute(path)
+      ? path
+      : node.path.join(node.process.cwd(), path)
+
+    const a4t1Folder = node.path.join(cwd, '.a4t1/')
+
+    let authToken = undefined
+    try {
+      authToken = token || node.fs.readFileSync(node.path.join(cwd, 'LUK3_TOK3N'), { encoding: 'utf8' })
+    }
+    catch (err) {
+      throw new Error(`Token di autenticazione non disponibile!
+  Assicurarsi di avere copiato il file LUK3_TOK3N nella cartella principale di
+  MOHAA, oppure utilizzare l'opzione --token`)
+    }
+
+    const pacoteOptions = {
+      registry: 'https://npm.pkg.github.com/',
+      token: authToken,
+      cache: node.path.join(a4t1Folder, 'cache'),
+      preferOffline: true,
+    }
+
+    if (!node.fs.existsSync(node.path.join(cwd, 'MOHAA.exe'))) {
+      if (!force) {
+        throw new Error(`MOHAA.exe non trovato!
+  Esegui il comando nella cartella principale di MOHAA o usa l'opzione --path`)
+      }
+    }
+
+    const url = await pacote.resolve(`@a4t1/luk3-config@latest`, pacoteOptions)
+
+    console.log(`Download configurazione da ${pacoteOptions.registry}...`)
+
+    const downloadFolder = node.path.join(a4t1Folder, 'luk3-config')
+    await pacote.extract(url, downloadFolder, pacoteOptions)
+
+    const configuration = require(node.path.join(downloadFolder, 'config.js'))
+
+    for (const bundle of configuration.bundles) {
+      await bootstrapBundle(bundle, cwd, pacoteOptions)
+    }
+
+    console.log()
+    console.log(`Operazione completata`)
+    console.log()
+
+    await confirmExit()
+
+    node.process.exit(0)
+
+    return 0
   }
   catch (err) {
-    throw new Error(`Token di autenticazione non disponibile!
-Assicurarsi di avere copiato il file LUK3_TOK3N nella cartella principale di
-MOHAA, oppure utilizzare l'opzione --token`)
+
+    console.error(`Si è verificato un errore`)
+    console.error(err)
+    console.log()
+
+    await confirmExit()
+
+    node.process.exit(-1)
   }
+}
 
-  const pacoteOptions = {
-    registry: 'https://npm.pkg.github.com/',
-    token: authToken,
-    cache: node.path.join(a4t1Folder, 'cache'),
-    preferOffline: true,
-  }
-
-  if (!node.fs.existsSync(node.path.join(cwd, 'MOHAA.exe'))) {
-    if (!force) {
-      throw new Error(`MOHAA.exe non trovato!
-Esegui il comando nella cartella principale di MOHAA o usa l'opzione --path`)
-    }
-  }
-
-  const url = await pacote.resolve(`@a4t1/luk3-config@latest`, pacoteOptions)
-
-  console.log(`Download configurazione da ${pacoteOptions.registry}...`)
-
-  const downloadFolder = node.path.join(a4t1Folder, 'luk3-config')
-  await pacote.extract(url, downloadFolder, pacoteOptions)
-
-  const configuration = require(node.path.join(downloadFolder, 'config.js'))
-
-  for (const bundle of configuration.bundles) {
-    await bootstrapBundle(bundle, cwd, pacoteOptions)
-  }
-
-  console.log(`
-Operazione completata`)
-
-  node.process.exit(0)
-
-  return 0
-
+async function confirmExit() {
+  const response = await prompts({
+    type: 'invisible',
+    name: 'value',
+    message: 'Premi INVIO per uscire'
+  })
+  return response
 }
 
 async function bootstrapBundle({ name: bundleName, targets, pak3ts }, cwd, pacoteOptions) {
